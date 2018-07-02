d734cbd638f72064177949fc1329f7b7fd2c339d
Shre23 <jain.shreyansh0@gmail.com>
2018-07-02 19:36:07 +0530

    added user-registration

--- backend/stream/profiles_api/serializers.py
+++ backend/stream/profiles_api/serializers.py
@@ -2,7 +2,22 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email',)
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
+        write_only_fields = ('password',)
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
